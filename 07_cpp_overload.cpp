5edd9a9951b9d9209a1118586ed473c5d0e5025c
Gangfeng Xu <gangfeng.xu@amlogic.com>
2015-04-21 11:53:51 +0800

    add cpp overload function test module

---
+++ 07_cpp_overload.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+    > File Name: 07_cpp_overload.cpp
+    > Author: koffuxu
+    > Mail: koffuxu@gmail.com 
+    > Created Time: Mon 20 Apr 2015 11:09:45 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+void print(int i)
+{
+    cout<<"Print a integer:"<<i<<endl;
+}
+void print(string str)
+{
+    cout<<"print a Streing:"<<str<<endl;
+}
+int main(){
+    print(10);
+    print("Hello Test CPP Overload!");
+    return 0;
+}
